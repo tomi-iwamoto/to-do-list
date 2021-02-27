d9a93af6faaa17bedd5d0b2c82fa47c775669e01
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-26 21:36:26 -0500

    Tried to separate event listeners and added notes where it did not work, returned code to the way it was previously.

--- script.js
+++ script.js
@@ -1,81 +1,86 @@
 //When I type a text in to the input field, the word I typed will show up on the screen when I click the "add item" button
 
-let addButton = document.getElementById('add');
-let clearButton = document.getElementById('clear');
-let input = document.getElementById('activity');
+let addButton = document.getElementById('add');
+let clearButton = document.getElementById('clear');
+let input = document.getElementById('activity');
 
 
-input.addEventListener('keyup', function(e) {
+input.addEventListener('keyup', function (e) {
 
     //This indicates that the event will happen when the key is the enter key
-    if(e.keyCode == 13) {
-    //Getting the input field
-    let input = document.getElementById('activity');
-    //This is the value of the input field- whatever you type into the field 
-    let activityName = input.value;
-    //Getting at the list container
-    let container = document.getElementById('list-container');
-    //Creating a new div in order to attach to the list container
-    let div = document.createElement('div');
-    //Attaching the new div to the list container. 
-    container.append(div);
-    //The inner text of the new div will be the activityName
-    div.innerText = activityName;
-    //Once the input field has been submitted by this button, the input value is going to be clear
-    input.value = ' ';
+    if (e.keyCode == 13) {
+
+        //Getting the input field
+        let input = document.getElementById('activity');
+        //This is the value of the input field- whatever you type into the field 
+        let activityName = input.value;
+        //Getting at the list container
+        let container = document.getElementById('list-container');
+        //Creating a new div in order to attach to the list container
+        let div = document.createElement('div');
+        //Attaching the new div to the list container. 
+        container.append(div);
+        //The inner text of the new div will be the activityName
+        div.innerText = activityName;
+        //Once the input field has been submitted by this button, the input value is going to be clear
+        input.value = ' ';
 
 
-    //Adding styles to this div
-    div.classList.add('newDiv');
+        //Adding styles to this div
+        div.classList.add('newDiv');
+        div.id = "newDivId";
 
-    //Creating a second div to place inside of the original div to use as a wrapper for the icons
-    let innerDiv = document.createElement('div');
-    div.appendChild(innerDiv);
+        //Creating a second div to place inside of the original div to use as a wrapper for the icons
+        let innerDiv = document.createElement('div');
+        div.appendChild(innerDiv);
 
-    //Creating icon to clear individual item
-    //To do this, create an image element and then set the attributes of the image using setAttribute
-    let check = document.createElement('IMG');
-    check.setAttribute('src', 'check-mark.png')
-    check.setAttribute('width', '20');
-    check.setAttribute('height', '20');
-    check.style.margin = "10px";
-    
-    //Adding the icon to the inner div
-    innerDiv.appendChild(check);
-    
-    //When the image element (icon) has been clicked, it will put a line through the the div
-    check.addEventListener('click', function() {
+        //Creating icon to put a line through activities that are completed.
+        //To do this, create an image element and then set the attributes of the image using setAttribute
+        //I needed to do this inside of the keyup event listener because the check mark has to be created along with the new div. 
+        let check = document.createElement('IMG');
+        check.setAttribute('src', 'check-mark.png')
+        check.setAttribute('width', '20');
+        check.setAttribute('height', '20');
+        check.style.margin = "10px";
+        check.id = "checkId";
 
-        div.classList.toggle('lineThrough');
+        //Adding the icon to the inner div
+        innerDiv.appendChild(check);
 
-    })
+        //When the image element (icon) has been clicked, it will put a line through the the div
+        check.addEventListener('click', function () {
 
-    //Adding a close icon
-    let close = document.createElement('IMG');
-    close.setAttribute('src', 'close.png');
-    close.setAttribute('width', '20');
-    close.setAttribute('height', '20');
-    close.style.margin = "10px";
+            div.classList.toggle('lineThrough');
+
+        })
 
 
-    //Adding the icon to the added div
-    innerDiv.appendChild(close);
+        //Adding a close icon
+        let close = document.createElement('IMG');
+        close.setAttribute('src', 'close.png');
+        close.setAttribute('width', '20');
+        close.setAttribute('height', '20');
+        close.style.margin = "10px";
+        //Adding the icon to the added div
+        innerDiv.appendChild(close);
 
-    //When the close button is clicked, it will remove the added div
-    close.addEventListener('click', function() {
+        //When the close button is clicked, it will remove the added div
+        close.addEventListener('click', function () {
 
-        div.style.display = 'none';
+            div.style.display = 'none';
 
-    })
-    
-}
+        })
+
+
+
+    }
 
 })
 
 
 
 //This clear button will erase all entries
-clearButton.addEventListener('click', function() {
+clearButton.addEventListener('click', function () {
 
     //Getting at the div elements with the class of "add"
     let addedItems = document.querySelectorAll('.newDiv');
@@ -83,7 +88,7 @@ clearButton.addEventListener('click', function() {
     let addedItemsArray = Array.from(addedItems);
 
     //For each of the added divs with the class of "add",run this function- This will remove the item from the screen
-    addedItems.forEach(function(target) {
+    addedItems.forEach(function (target) {
 
         target.style.display = 'none';
 
@@ -91,3 +96,22 @@ clearButton.addEventListener('click', function() {
 
 
 })
+
+
+
+
+/*Tried to separate event listeners -- I tried to access the dynamically created elements by ids, but it would not work. Therefore I am returning this event listener back to its position inside of the other event listener until I can figure out why!
+
+let checkIcon = document.getElementById('checkId');
+let newDivId = document.getElementById('newDivId');
+
+//When the image element (icon) has been clicked, it will put a line through the the div
+checkIcon.addEventListener('click', function () {
+
+    newDivId.classList.toggle('lineThrough');
+
+})*/
+
+
+
+
